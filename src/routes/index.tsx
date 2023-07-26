faec9b5007b510b6050022435b5ba56b8b98b30d
Maik-Biazi <maikwilliam4553@gmail.com>
2023-07-25 21:04:09 -0300

    finalizando o theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 import {Routes, Route,Navigate} from 'react-router-dom'
+import { useAppThemeContext } from '../shared/contexts'
 export const AppRoutes = ()=> {
+    const {toggleTheme}= useAppThemeContext()
     return (
         <Routes>
             <Route path='/pagina-inicial' element={<p>Pagina Inicial</p>}/>
