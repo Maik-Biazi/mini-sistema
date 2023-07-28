ef759c0549e9a189e5ae4ff542f09481ec6f02a4
Maik-Biazi <maikwilliam4553@gmail.com>
2023-07-27 22:14:52 -0300

    finalizando a lateral

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,27 +1,51 @@
-import { Avatar, Box, Drawer, useTheme, Divider } from "@mui/material"
-import { deepOrange } from "@mui/material/colors"
+import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
+import { Box } from '@mui/system';
 
-interface IMenuLateralProps {
-    children: React.ReactNode
+import {useDrawerContext } from '../../contexts';
+
+interface IMenulateralProps{
+    children: React.ReactNode;
+
 }
 
-export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
-    const theme = useTheme()
-    return (
-        <>
-            <Drawer variant="permanent">
-                <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
-                    <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
-                        <Avatar sx={{ bgcolor: deepOrange[500], height: theme.spacing(12), width: theme.spacing(12) }}>M</Avatar>
-                    </Box>
-                    <Divider />
-                    <Box flex={1}>
-                    </Box>
-                </Box>
-            </Drawer>
-            <Box height="100vh" marginLeft={theme.spacing(28)}>
-                {children}
-            </Box>
-        </>
-    )
-}
+
+export const MenuLateral: React.FC<IMenulateralProps> = ({ children }) => {
+  const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
+
+  return (
+    <>
+      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
+        <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+
+          <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              src="https://yt3.ggpht.com/grfYgQadT8iNg9WPb-jkrKB-9224y_DBDXAOtV4Yt7cyQmtR47J_453uveQOTDsp_dRSH851TMM=s108-c-k-c0x00ffffff-no-rj"
+            />
+          </Box>
+
+          <Divider />
+
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Página inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+
+        </Box>
+      </Drawer>
+
+      <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
+};
